446e369937db5903d43cb920a68bd21693287e5a
colton <lwcolton@gmail.com>
2016-02-12 17:52:54 -0600

    docker-release will update master

--- docker-release.sh
+++ docker-release.sh
@@ -1,3 +1,7 @@
 aws ecr get-login --region us-east-1
 docker tag -f python3-base:develop 607520688049.dkr.ecr.us-east-1.amazonaws.com/python3-base:latest
 docker push 607520688049.dkr.ecr.us-east-1.amazonaws.com/python3-base:latest
+git fetch origin
+git checkout master
+git merge develop
+git push origin master
