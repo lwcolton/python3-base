75f67956a07f651bb31866cd81a9bab75006b07a
colton <lwcolton@gmail.com>
2016-02-12 17:51:27 -0600

    separate docker build and release

---
+++ docker-release.sh
@@ -0,0 +1,3 @@
+aws ecr get-login --region us-east-1
+docker tag -f python3-base:develop 607520688049.dkr.ecr.us-east-1.amazonaws.com/python3-base:latest
+docker push 607520688049.dkr.ecr.us-east-1.amazonaws.com/python3-base:latest
