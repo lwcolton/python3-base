1c2595b2e2a6b87bfea0250e93d5962c6ab3ee54
colton <lwcolton@gmail.com>
2016-02-12 18:52:38 -0600

    try git push origin HEAD:master

--- docker-release.sh
+++ docker-release.sh
@@ -2,4 +2,4 @@ docker_login_cmd=`aws ecr get-login --region us-east-1`
 $docker_login_cmd
 docker tag -f python3-base:develop 607520688049.dkr.ecr.us-east-1.amazonaws.com/python3-base:latest
 docker push 607520688049.dkr.ecr.us-east-1.amazonaws.com/python3-base:latest
-git push origin $BUILDKITE_BRANCH:master
+git push origin HEAD:master
